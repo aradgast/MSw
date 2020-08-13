efa01311f70fe5150f045c3d5318c731e493bbb2
aradgast <66379566+aradgast@users.noreply.github.com>
2020-08-13 18:24:44 +0300

    Update users.py

--- users.py
+++ users.py
@@ -1,5 +1,5 @@
 USERS = {
     'Arad Gast': 'aradgast1@gmail.com',
-#     'Omer': 'Omerts11@gmail.com',
-#     'Ofek': 'Ofekmaman333@gmail.com', 
+    'Omer': 'Omerts11@gmail.com',
+    'Ofek': 'Ofekmaman333@gmail.com', 
 }
