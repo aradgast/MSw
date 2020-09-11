3e372bb65f37b843d14a3caa89be5f3bf8eace5a
koren.gast <koren.gast@playtech.com>
2020-09-11 17:44:28 +0300

    [WIP]

--- Google_docs/fetch_data.py
+++ Google_docs/fetch_data.py
@@ -1,19 +1,51 @@
-import gspread
-from oauth2client.client import SignedJwtAssertionCredentials
-import pandas as pd
-import json
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
 
-SCOPE = ["https://spreadsheets.google.com/feeds"]
-SECRETS_FILE = "Waves_update.json"
-SPREADSHEET = "Waves update registration (Responses)"
+# If modifying these scopes, delete the file token.pickle.
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
 
-json_key = json.load(open(SECRETS_FILE))
-# Authenticate using the signed key
-credentials = SignedJwtAssertionCredentials(json_key['client_email'],
-                                            json_key['private_key'], SCOPE)
+# The ID and range of a sample spreadsheet.
+SPREADSHEET_ID = '1iwlsnR8GI2XBBIQd5AJrIkAC5ALuDMrQZU-YrWZ7wGc'
 
-gc = gspread.authorize(credentials)
+def main():
+    """Shows basic usage of the Sheets API.
+    Prints values from a sample spreadsheet.
+    """
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'client_id.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+    service = build('sheets', 'v4', credentials=creds)
 
-print("The following sheets are available")
-for sheet in gc.openall():
-    print("{} - {}".format(sheet.title, sheet.id))
+    # Call the Sheets API
+    sheet = service.spreadsheets()
+    result = sheet.values().get(spreadsheetId=SPREADSHEET_ID).execute()
+    values = result.get('values', [])
+
+    if not values:
+        print('No data found.')
+    else:
+        print('Name, Major:')
+        for row in values:
+            # Print columns A and E, which correspond to indices 0 and 4.
+            print('%s, %s' % (row[0], row[4]))
+
+if __name__ == '__main__':
+    main()
