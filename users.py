5aa360af2a296eba61f3900402cf43420cb2e962
aradgast <aradgast1@gmail.com>
2020-09-11 16:41:10 +0300

    Git learning

--- users.py
+++ users.py
@@ -2,4 +2,6 @@ USERS = {
     'Arad Gast': 'aradgast1@gmail.com',
     'Omer': 'Omerts11@gmail.com',
     'Ofek': 'Ofekmaman333@gmail.com', 
+    'Koren': 'O@gmail.com',
+    'oren': 'O@gmail.com'
 }
