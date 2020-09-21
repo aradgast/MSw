8f720afeff4a6a7f376c68da0eecb2e0e536c99a
aradgast <66379566+aradgast@users.noreply.github.com>
2020-09-21 11:50:37 +0300

    Update get_data.py
    test

--- get_data.py
+++ get_data.py
@@ -64,7 +64,7 @@ class Msw:
         '''when init the repeat_bool field as True, calling this func will opreate the update func in interval time periods'''
         while self.repeat_bool:
             self.update()
-            time.sleep(60*60*16)
+            time.sleep(15)
 
     def to_dataframe(self, url):
         return pd.DataFrame((pd.read_json(url.text)))
