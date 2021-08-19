437692445ef3523c2f882a3b22ad5e490bf8ab72
aradgast <aradgast1@gmail.com>
2021-08-19 18:07:26 +0300

    [WIP]

--- get_data.py
+++ get_data.py
@@ -186,8 +186,8 @@ class Msw:
 if __name__ == '__main__':
     print('***************************')
     a = Msw(repeat_bool=True)
-    # a.repeat()
-    a.update()
+    a.repeat()
+    # a.update()
     # telegram_token = '1393856489:AAFdXkyWqrivY8PVKF9AC8modSJMY0G_IQo'
     # chat_id = '1393856489'
     # print(datetime.today().strftime('%A'))
